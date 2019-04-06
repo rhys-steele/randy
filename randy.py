c6d30711de2dc6d42f036a64d11dabc40a5ad87d
Rhys May <rhys@rswm.com.au>
2019-04-06 18:29:05 +1000

    Fixing issues

--- randy.py
+++ randy.py
@@ -7,7 +7,7 @@ enA = 18 # Broadcom pin 13 (P1 pin 33)
 inA1 = 23 # Broadcom pin 19 (P1 pin 35)
 inA2 = 17 # Broadcom pin 26 (P1 pin 37)
 
-dc = 95 # duty cycle (0-100) for PWM pin
+dc = 5 # duty cycle (0-100) for PWM pin
 
 # Pin Setup:
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
@@ -16,15 +16,14 @@ GPIO.setup(inA1, GPIO.OUT) # inA1 pin set as output
 GPIO.setup(inA2, GPIO.OUT) # inA2 pin set as output
 GPIO.setup(enA, GPIO.OUT) # PWM pin set as output
 
-# pwm = GPIO.PWM(enA, 50)  # Initialize PWM on enA 100Hz frequency
+pwm = GPIO.PWM(enA, 50)  # Initialize PWM on enA 100Hz frequency
 
 count = 0 # Initialize count
 
 # Initial state for LEDs:
 GPIO.output(inA1, GPIO.LOW)
 GPIO.output(inA2, GPIO.HIGH)
-GPIO.output(enA, GPIO.HIGH)
-# pwm.start(dc)
+pwm.start(dc)
 
 print("Here we go! Press CTRL+C to exit")
 try:
@@ -37,4 +36,5 @@ except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
     pwm.stop() # stop PWM
     GPIO.cleanup() # cleanup all GPIO
 
+pwm.stop() # stop PWM
 GPIO.cleanup() # cleanup all GPIO
