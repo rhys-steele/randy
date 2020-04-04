b03bec5428f557905d5e60f054848575d3d5380e
Rhys May <rhys@rswm.com.au>
2020-04-05 02:57:51 +1100

    Adding controller

--- randy-laravel/app/Http/Controllers/RandyController.php
+++ randy-laravel/app/Http/Controllers/RandyController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class RandyController extends Controller
 {
+    public function setup(Request $request)
+    {
+        Artisan::call('pin:mode', [
+            'pin' => 27,
+            'mode' => 'output'
+        ]);
+        Artisan::call('pin:mode', [
+            'pin' => 28,
+            'mode' => 'output'
+        ]);
+        Artisan::call('pin:mode', [
+            'pin' => 29,
+            'mode' => 'output'
+        ]);
+        return response()->json([
+            'success' => true
+        ], 200);
+    }
+
     public function execute(Request $request)
     {
         $validated = $request->validate([
